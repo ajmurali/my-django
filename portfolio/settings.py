ae59984dd2754412be9044a27e533203a1afb29d
ajmurali <109534812+ajmurali@users.noreply.github.com>
2023-08-02 09:45:14 +0530

    updated urls.py, allowed hosts,requirements.txt,vercel.json, created build_files.sh

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-f92f6!ry4&39e&zzp(yic!k=pgu33y6j+*tt148&g4+zapd4+z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','localhost', '.now.sh']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , "media/")
@@ -146,6 +147,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/")
 ]
+STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
